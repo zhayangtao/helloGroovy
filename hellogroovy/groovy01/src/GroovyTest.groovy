8c6a0d0776da43bc9c60347d3047c93316bb9599
zhayangtao <15313039096@163.com>
2017-04-17 15:30:45 +0800

    my first groovy files

---
+++ hellogroovy/groovy01/src/GroovyTest.groovy
@@ -0,0 +1,29 @@
+/**
+ * Created by shliangyan on 2017/4/17.
+ */
+class GroovyTest {
+    def name
+    def age
+    String toString(){
+        "$name,$age"
+    }
+    def test =
+            println "hello groovy"
+
+    static void main(String[] args) {
+        def map = ['name': 'john', 'age': 14, 'sex': 'm']
+        map= map + ['weight':50]
+        map.each {key,value -> println "$key:,$value"}
+        map.each {println it}
+        map.each{println it.getKey() + "-->" + it.getValue()}
+
+        GroovyTest gt = new GroovyTest();
+        gt.say('groovy')
+        gt.setName 'kk'
+        println gt.getName()
+    }
+
+    def say = {word -> println "hi,$word!"}
+
+
+}
